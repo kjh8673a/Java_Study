029a7f51e2e274bedcf3b99bdda4e5179683ec2d
kjh8673a <kjh8673a@naver.com>
2022-01-16 22:31:23 +0900

    220116

---
+++ 01 입출력과 사칙연산/src/Baekjoon_10172.java
@@ -0,0 +1,20 @@
+/*
+문제번호: 10172
+제목: 개
+문제: 아래 예제와 같이 개를 출력하시오.
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+*/
+
+public class Baekjoon_10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\"); // \, ', " 를 출력하고 싶으면 앞에 \를 붙여 출력한다.
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
